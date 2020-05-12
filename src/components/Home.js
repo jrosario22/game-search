530e0ca7c64b4dcb7304a8f72e7d9d58eb3e9495
Jeson Rosario <jesonrosario22@gmail.com>
2020-05-12 16:46:25 -0400

    Created the initial components for app

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcomce to Game-Search</h1>
+      <h2>Find over 350,000+ titles!</h2>
+    </div>
+  );
+}
