dcc8c8c3c240f71281fdae5d0342c248f13b5cf3
Jeson Rosario <jesonrosario22@gmail.com>
2020-05-22 23:01:16 -0400

    Fixed typo on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcomce to Game-Search</h1>
+      <h1>Welcome to Game-Search</h1>
       <h2>Find over 350,000+ titles!</h2>
+      <SearchBar />
     </div>
   );
 }
