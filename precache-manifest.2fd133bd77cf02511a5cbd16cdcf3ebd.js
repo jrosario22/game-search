53426ad6f7fdc2c99a889f8b9e685ccc2e09f490
Jeson Rosario <jesonrosario22@gmail.com>
2020-05-24 19:12:52 -0400

    Updates

---
+++ precache-manifest.2fd133bd77cf02511a5cbd16cdcf3ebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9c14440539c3bb01378907dfec907a",
+    "url": "/game-search/index.html"
+  },
+  {
+    "revision": "c94fcddbe9a7da252e16",
+    "url": "/game-search/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "a2f9216d304e1e178fc6",
+    "url": "/game-search/static/css/main.544a1999.chunk.css"
+  },
+  {
+    "revision": "c94fcddbe9a7da252e16",
+    "url": "/game-search/static/js/2.57415efe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-search/static/js/2.57415efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f9216d304e1e178fc6",
+    "url": "/game-search/static/js/main.f89b5ddc.chunk.js"
+  },
+  {
+    "revision": "6054a0717ac860162336",
+    "url": "/game-search/static/js/runtime-main.6758d0cb.js"
+  }
+]);
